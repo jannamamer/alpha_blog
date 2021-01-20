31724b26988e2e08b2ab948240d79fcce9871ca4
Janna Dela Cruz <53164350+jannamamer@users.noreply.github.com>
2021-01-20 13:15:20 +1300

    Fix issue with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-unless User.find(email: "janna@example.com")
+# Seed Gender
+[
+  { group: "gender", name: "Female" },
+  { group: "gender", name: "Male" },
+  { group: "gender", name: "Something else" }
+].each do |look_up| 
+  LookUp.create(group: look_up[:group], name: look_up[:name]) unless LookUp.exists?(group: look_up[:group], name: look_up[:name])
+end
+
+# Seed user
+unless User.find_by(email: "janna@example.com")
   User.create(email: "janna@example.com",
               username: "janna",
               first_name: "Janna",
               last_name: "Dela Cruz",
-              password: "password"
+              password: "password",
+              gender: LookUp.find_by(group: "gender", name: "Female").id
              )
-end
-
-[
-  %{
-    type: "gender",
-    name: "Female",
-  },
-  %{
-    type: "gender",
-    name: "Male"
-  },
-  %{
-    type: "gender",
-    name: "Something else"
-  }
-].each do |type, name|
-  unless LookUp.where(type: type, name: name) do
-    LookUp.create(type: type, name: name)
-  end
 end
