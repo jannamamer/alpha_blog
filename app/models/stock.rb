bd0589d84a2d4543308a28822f43fbf8d984deaa
Janna Dela Cruz <53164350+jannamamer@users.noreply.github.com>
2021-02-12 21:52:04 +1300

    Adds method to fetch stock price from API

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    iex_client_credentials = Rails.application.credentials.iex_client
+
+    client = IEX::Api::Client.new(
+      publishable_token: iex_client_credentials[:sandbox_api][:publishable_token],
+      secret_token: iex_client_credentials[:sandbox_api][:secret_token],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    client.price(ticker_symbol)
+  end
 end
