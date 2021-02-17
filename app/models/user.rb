35cf7b6ad17c93c72e4fd466487686c0a591a3b5
Janna Dela Cruz <53164350+jannamamer@users.noreply.github.com>
2021-02-17 20:55:51 +1300

    Adds restriction when adding stock

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,18 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }
 
   self.per_page = 3
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
